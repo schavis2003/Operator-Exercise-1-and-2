625c0184b6e931dcc9804fd684a59f1152cca01a
schavis2022 <schavis2022>
2022-06-15 03:13:44 -0400

    Operation Exercise 1 and 2

---
+++ source/repos/OperatorExercise/OperatorExercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //OperatorExercise1
+            //For division, create two integer variables called a and b.
+            
+
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+           //Console.WriteLine(a + " / " + b + " is " + quotient +  + " remainder " + remainder); This line kept giving me error, so I wrote the next line
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("Please enter the radius of your circle");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle);
+        }
+
+        //OperationExercise2 - define a method - which performs some functioality, that can be used over and over again.
+
+        public static double AreaOfCircle(double radius) 
+        {
+            //This is the scope of the method
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return Math.Sqrt(area);
+        }
+
+    }
+
+
+}
