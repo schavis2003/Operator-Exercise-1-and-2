0160f9379f52dd2da2b6dca812c786c6b7269f0f
schavis2022 <schavis2022>
2022-06-30 17:24:05 -0400

    Methods with loops and Conditional 4

---
+++ source/repos/MethodWithLoopsAndConditionals 4/MethodWithLoopsAndConditionals 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MethodWithLoopsAndConditionals_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+                // Check if number is odd or even
+                if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is an even number.");
+            } 
+
+            
+                else {
+                Console.WriteLine($"{number} is an odd number.");
+            }
+        }
+    }
+}
