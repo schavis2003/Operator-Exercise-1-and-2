a2352300371b9935a39596f1ce9378033f086b7b
schavis2022 <schavis2022>
2022-06-30 18:20:48 -0400

    Positive and Negative

---
+++ source/repos/MethodsAndLoopsWithConditionals Positive and Negative/MethodsAndLoopsWithConditionals Positive and Negative/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MethodsAndLoopsWithConditionals_Positive_and_Negative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a method to check whether a given number is positive or negative
+
+            int a;
+            Console.WriteLine("Input Any Number");
+            a = int.Parse(Console.ReadLine());
+
+            if (a >= 0)
+            {
+                if (a == 0)
+                {
+                    Console.WriteLine("Entered Number is zero");
+                }
+                else
+                {
+                    Console.WriteLine(a + "is a positive number");
+                
+                else
+                 
+                    Console.WriteLine(a + "is negative number");
+                
+            
+   
